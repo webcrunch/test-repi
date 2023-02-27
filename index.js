4bf2bc497e9d90967e0647b6ffb992aa7ef7fee9
jarl Lindquist <jarllinduist@gmail.com>
2023-02-27 15:11:21 +0100

    more structural stigenfff

---
+++ index.js
@@ -0,0 +1,24 @@
+// Port to run on
+const port = 4000;
+
+import express from "express";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import * as fs from 'fs/promises';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootPath = path.normalize(__dirname.split("src")[0]);
+
+// Create a web server
+const app = express();
+
+// Serve all the files in the dist folder
+app.use(express.static('dist'));
+
+// Always serve index.html if nothing else found
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Start the web server on a port
+app.listen(port, () => console.log('Listening on port ' + port));
