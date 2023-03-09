248ea54b0e704c29a77f5e185fbfaca0da982665
webcrunch <jarllinduist@gmail.com>
2023-03-09 18:17:06 +0100

    removinder

--- src/App.svelte
+++ src/App.svelte
@@ -20,13 +20,7 @@
 
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>  
-
-  <p class="read-the-docs">
-    This will create a changes to the git reposioriet and it will be an exploation
-  </p>
-
-  
+  </p>    
   
 </main>
 
