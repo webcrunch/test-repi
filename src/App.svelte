465b07570e9d2773cd47a5f1858952f0d76c0e73
webcrunch <jarllinduist@gmail.com>
2023-03-09 18:25:34 +0100

    none

--- src/App.svelte
+++ src/App.svelte
@@ -20,7 +20,7 @@
 
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>    
+  </p> 
   
 </main>
 
